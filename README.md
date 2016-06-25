14c947df3db1ec57f1a745ace0435bec19cbebdd
Preston <ptkresch@gmail.com>
2016-06-24 21:49:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+#Udacity Feedreader Testing Project
+
+##Introduction
+
+A premade newsfeed application was put through a series of tests using [Jasmine](http://jasmine.github.io/).
+
+##Running the Application
+
+###Downloading the application
+Begin by navigating [here](https://github.com/ptkresch/FeedreaderTesting) to find the Github repository for the application. Select the 'Clone or download' button and choose one of the options: Open in Github desktop, Download ZIP file, or copy the git url to the clipboard. The latter utilizes Github through the terminal or command line. 
+
+If the ZIP file is download, simply extract the folder using an unarchiver program and browse the folder for a file named 'index.html'. Open this file using a web browser (Chrome recommended) and the app will start.
+
+If using Github through the terminal, navigate to the folder you would like to clone the repository to. Then enter the following: 
+
+`$ git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY`
+
+The above url is the one copied to the clipboard from the main repository page, so simply paste the url after typing 'git clone'. Following this, a local repository should be created with all the files necessary to run the app. Navigate to the folder and open 'index.html' inside of it with a web browser (again, Chrome recommended). 
+
+###Testing the application on a server
+
+Although not really necessary for this project, the steps below outline the method used to run this application on a server.
+
+Download [ngrok](https://ngrok.com/) and open terminal. Unzip the file using the command: 
+
+`$ unzip /path/to/ngrok.zip`
+
+Open another Terminal window or tab and enter the following:
+
+`python -m SimpleHTTPServer YOURPORTNUMBERHERE`
+
+Enter an appropriate number for the port your server will run on (the defualt usually being port 80, according the the official documentation found [here](https://ngrok.com/docs#expose)). Provided the ngrok file is placed in the repository for the portfolio, ngrok can be initialized with the following command in a separate terminal window:
+
+`./ngrok http YOURPORTNUMBERHERE`
+
+When ngrok starts, it will display a url of the tunnel. Copy the url provided and enter it into a web browser to view the app. Now, the application can be tested online.
+
+##Tests
+1. News Feeds
+	1. Each newsfeed object in the allFeeds object has a defined url that is not empty.
+	2. Each newsfeed object in the allFeeds object hs a defined name that is not empty.
+2. Menu Functionality
+	1. The menu is hidden by default
+	2. The menu changes visibility when clicked - It appears on the first click and hides on the second.
+	3. The menu position changes upon each click.
+3. Initial Entries
+	* The loadFeed function ensures that there is at least one .entry element in the .feed container. This required callbacks to ensure feeds are loaded before testing, as the feeds were fetched asynchronously through ajax requests.
+4. New Feed Selection
+	* Page content actually changes upon execution of the loadFeed function. Again, this required callbacks due to asynchronous ajax requests.
+
+In addition, tests were ensured to have no dependencies with one another.
+=======
+# FeedreaderTesting
+Udacity testing project - Feedreader application tested through Jasmine
+>>>>>>> 883b3d3683422f23707ace15d99bdb1697aae028
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,146 @@
+/* feedreader.js*/
+$(function() {
+
+    describe('RSS Feeds', function() {
+
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        it('have defined, non-empty urls', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                (function (feed) {
+                    var url = feed.url;
+                    expect(url).toBeDefined();
+                    expect(typeof(url)).toEqual("string");
+                    expect(url.length).toBeGreaterThan(0);
+                })(allFeeds[i]);
+            };
+        })
+
+        it('have defined, non-empty names', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                (function (feed) {
+                    var name = feed.name;
+                    expect(name).toBeDefined();
+                    expect(typeof(name)).toEqual("string");
+                    expect(name.length).toBeGreaterThan(0);
+                })(allFeeds[i]);
+            };
+        })
+    });
+
+    describe('The menu', function() {
+        var body = $('body');
+        var $menuButton = $('.menu-icon-link');
+        var $menuPosition;
+
+        it('is hidden by default', function() {
+            var menuPosition = $('.slide-menu').position().left;
+            expect(body.hasClass('menu-hidden')).toBe(true);
+            //Menu position -192 obtained through devtools and the css stylesheet for the app.
+            expect(menuPosition).toBe(-192);
+        });
+
+        describe('The menu will be', function() {
+
+            it('visible when the menu button is clicked once', function() {
+                $menuButton.trigger('click');
+                expect(body.hasClass('menu-hidden')).toEqual(false);
+                describe('but is hidden')
+            })
+
+            it('hidden when the menu button is clicked again', function() {
+                $menuButton.trigger('click');
+                expect(body.hasClass('menu-hidden')).toEqual(true);         
+            })
+        });
+
+        it('moves offscreen', function(done) {
+            $menuButton.trigger('click');
+            //Due to the transition effect, multiple timeouts are requrired to ensure that
+            //expect functions are called after the transition ends - otherwise the wrong
+            //position is reported.
+            window.setTimeout(function() {
+                $menuPosition = $('.slide-menu').position().left;
+                expect($menuPosition).not.toBeLessThan(0);
+                $menuButton.trigger('click');
+                window.setTimeout(function() {
+                    $menuPosition = $('.slide-menu').position().left;
+                    expect($menuPosition).toBeLessThan(0);
+                    done();
+                }, 1500)
+            }, 1500);
+        });
+    }); 
+
+    describe('Initial Entries', function() {
+        //Asynchronous request requires callback, or the test runs before the ajax request finishes.
+        loadFeed.initialComplete = false;
+        loadFeed.wait = function(cb) {
+            setTimeout(function() {
+                loadFeed.initialComplete = true;
+                if (cb) {
+                    return cb();
+                }
+            }, 3);
+        }
+        beforeEach(function(done){
+            loadFeed.wait(function() {
+                done();
+            });
+        });
+
+        it('at least one .entry element exists in the .feed container when loadFeed is called', function(done) {
+            loadFeed.wait();
+            var $feed = $('.feed');
+            var $entry = $('.entry');
+            expect(loadFeed.initialComplete).toBe(true);
+            //Loadfeed should ensure that there is at least on entry.
+            expect($entry.length).toBeGreaterThan(0);
+            //Loadfeed should ensure that the entry is added to the feed container.
+            expect($feed.length).toBeGreaterThan(0);
+            expect($feed.children()[0]).toBe($entry.parent()[0]);
+            done();
+        })
+    })
+
+    describe('New Feed Selection', function() {
+        loadFeed.initialComplete = false;
+        //Asynchronous request requires callback, or the test runs before the ajax request finishes.
+        loadFeed.wait = function(cb) {
+            setTimeout(function() {
+                loadFeed.initialComplete = true;
+                if (cb) {
+                    return cb();
+                }
+            }, 2);
+        }
+        beforeEach(function(done){
+            //There was an error due to jasmine's default timeout - changing the default fixes this.
+            window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+            loadFeed.wait(function(){
+                done();
+            });
+            spyOn(loadFeed, 'wait');
+        });
+        it('content should change when new feed is loaded', function(done) {
+            loadFeed.wait();
+            //Ensure loadFeed.wait is called - that way the asynchronous request finishes.
+            expect(loadFeed.wait).toHaveBeenCalled();
+            var $feed = $('.feed');
+            var $entry = $('.entry');
+            expect(loadFeed.initialComplete).toBe(true);
+            for (var i = 0; i < allFeeds.length; i++) {
+                (function (entry) {
+                    //Each entry in allFeeds is added to the document.
+                    expect(jQuery.contains(document, entry));
+                })($entry[i]);
+            };
+            done();
+        })
+        
+    })
+
+}());
